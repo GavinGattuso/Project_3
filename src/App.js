0565108e2fdaaf98ab429a832f870ddaaca7a5e5
Austinjames99 <james.aj.austin@gmail.com>
2023-07-14 19:39:00 -0400

    add basic page layout and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-      <main>
-        <h1><span></span></h1>
-        <form></form>
-      </main>
+    <main>
+      <header>
+        <a href="" className='logo'>Blogger</a> 
+        <nav>
+          <a href=''>Login</a>
+          <a href=''>Register </a>
+        </nav>
+      </header>
+      <div className='post'>
+        <div className='image'>
+          <img src='https://cdn.geekwire.com/wp-content/uploads/2023/07/RFDiffusion_banner_2-630x354.jpeg'></img>
+        </div>
+        <div className='text'>
+          <h2>How AI is changing the way scientists engineer drugs, biosensors, enzymes and more</h2>
+          <p className='info'>
+            <span className='author'> Austin James</span>
+            <time>07-14-2023 7:29pm</time>
+          </p>
+          <p className='summary'>BOT or NOT? This special series explores the evolving relationship between humans and machines, examining the ways that robots, artificial intelligence and automation are impacting our work and lives.</p>
+        </div>
+      </div>
+      <div className='post'>
+        <div className='image'>
+          <img src='https://cdn.geekwire.com/wp-content/uploads/2023/07/RFDiffusion_banner_2-630x354.jpeg'></img>
+        </div>
+        <div className='text'>
+          <h2>How AI is changing the way scientists engineer drugs, biosensors, enzymes and more</h2>
+          <p className='info'>
+            <span className='author'> Austin James</span>
+            <time>07-14-2023 7:29pm</time>
+          </p>
+          <p className='summary'>BOT or NOT? This special series explores the evolving relationship between humans and machines, examining the ways that robots, artificial intelligence and automation are impacting our work and lives.</p>
+        </div>
+      </div>
+      <div className='post'>
+        <div className='image'>
+          <img src='https://cdn.geekwire.com/wp-content/uploads/2023/07/RFDiffusion_banner_2-630x354.jpeg'></img>
+        </div>
+        <div className='text'>
+          <h2>How AI is changing the way scientists engineer drugs, biosensors, enzymes and more</h2>
+          <p className='info'>
+            <span className='author'> Austin James</span>
+            <time>07-14-2023 7:29pm</time>
+          </p>
+          <p className='summary'>BOT or NOT? This special series explores the evolving relationship between humans and machines, examining the ways that robots, artificial intelligence and automation are impacting our work and lives.</p>
+        </div>
+      </div>
+    </main>
   );
 }
 
