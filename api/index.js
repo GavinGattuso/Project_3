70f93dc91763d4925120965b06bb9f20d9d6de62
Austinjames99 <james.aj.austin@gmail.com>
2023-07-15 11:52:10 -0400

    Register page authentication done

--- api/index.js
+++ api/index.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const User = require('./models/user')
+const bcrypt = require('bcryptjs') //password encryption
 const app = express()
+
+const salt = bcrypt.genSaltSync(10)//needed to make bcrypt work
 
 app.use(cors())
 app.use(express.json())
 
-app.post('/register', (req,res) => {
+mongoose.connect('mongodb+srv://newsapp:eM9QFIHWyEz2Wrtu@cluster0.e8evabe.mongodb.net/?retryWrites=true&w=majority')
+
+app.post('/register', async (req,res) => {
     const {username,password} = req.body
-    res.json({requestData:{username,password}})
+    try{
+        const userDoc = await User.create({
+            username, 
+            password:bcrypt.hashSync(password, salt), //password encrytpion
+        })
+        res.json(userDoc)
+    }   catch(e) {
+        console.log(e)
+        res.status(400).json(e) //Error handling if user is not unique
+    }
+    
     
 })
 app.listen(4000)
+
+//eM9QFIHWyEz2Wrtu
+//mongodb+srv://newsapp:eM9QFIHWyEz2Wrtu@cluster0.e8evabe.mongodb.net/?retryWrites=true&w=majority
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,8 +1,11 @@
 export default function LoginPage () {
+    const [username, setUsername] = usestate('')
+    const [password, setPassword] = useState('')
     return (
     <form className="login">
         <h1>Login</h1>
-        <input type='text' placeholder='username'/>
+        <input type='text' 
+        placeholder='username'/>
         <input type='password' placeholder='password'/>
         <button>Login</button>
     </form>
