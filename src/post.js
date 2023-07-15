5f83a59f75734aaeb1b861ddfe62da6ef5f7acb2
Austinjames99 <james.aj.austin@gmail.com>
2023-07-14 22:37:04 -0400

    add routes

---
+++ src/post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return(
+        <div className='post'>
+        <div className='image'>
+          <img src='https://cdn.geekwire.com/wp-content/uploads/2023/07/RFDiffusion_banner_2-630x354.jpeg'></img>
+        </div>
+        <div className='text'>
+          <h2>How AI is changing the way scientists engineer drugs, biosensors, enzymes and more</h2>
+          <p className='info'>
+            <span className='author'> Austin James</span>
+            <time>07-14-2023 7:29pm</time>
+          </p>
+          <p className='summary'>BOT or NOT? This special series explores the evolving relationship between humans and machines, examining the ways that robots, artificial intelligence and automation are impacting our work and lives.</p>
+        </div>
+      </div>
+    )
+}
